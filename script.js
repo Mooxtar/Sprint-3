f312639133ad1b46115da1d9099e173f40769c32
Mukhtar Rabayev <210103003@stu.sdu.edu.kz>
2023-03-14 14:25:53 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function func(){
+    let summa = document.getElementById('summa');
+    let x = document.getElementById('myselect');
+    let i = x.selectedIndex;
+    if(i == 0){
+        summa.value = 0;
+        document.getElementById('kzt').innerHTML = "-";
+        document.getElementById('usd').innerHTML = "-";
+        document.getElementById('eur').innerHTML = "-";
+    }
+    if(i == 1){
+        document.getElementById('kzt').innerHTML = "-";
+        document.getElementById('usd').innerHTML = (summa.value / 431).toFixed(2);
+        document.getElementById('eur').innerHTML = (summa.value / 509).toFixed(2);
+    }
+    if(i == 2){
+        document.getElementById('usd').innerHTML = "-";
+        document.getElementById('kzt').innerHTML = (summa.value * 429).toFixed(2);
+        document.getElementById('eur').innerHTML = (summa.value * 0.843).toFixed(2);
+    }
+    if(i == 3){
+        document.getElementById('eur').innerHTML = "-";
+        document.getElementById('kzt').innerHTML = (summa.value * 507).toFixed(2);
+        document.getElementById('usd').innerHTML = (summa.value * 1.176).toFixed(2);
+    }
+}
